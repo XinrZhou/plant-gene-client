92aa131186ca0d912da01c63614e26efa022c882
sfc9982 <26681591+sfc9982@users.noreply.github.com>
2023-03-20 20:54:31 +0800

    Pass Chart parameters

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,7 +6,7 @@
   </el-row>
   <el-row class="flex justify-center">
     <el-col :lg="12" :md="20" :sm="20">
-      <span class="bodyleft">{{homeContentData.bodyContent}}</span>
+      <span class="bodyleft">{{ homeContentData.bodyContent }}</span>
     </el-col>
     <el-col :lg="12" :md="20" :sm="20" class="bodyright">
       <div>
@@ -24,73 +24,77 @@
   <el-row>
     <el-col :span="24"><span class="data-text">Data Analysis</span></el-col>
   </el-row>
-  <el-row :gutter="20" class="flex justify-center" v-for="item in 2">
-    <el-col :lg="8" :md="12" :sm="16" v-for="item in 3">
-      <DataChart />
+  <el-row :gutter="20" class="flex justify-center" v-for="row in 2" :key="row">
+    <el-col :lg="8" :md="12" :sm="16" v-for="col in 3" :key="col">
+      <DataChart
+          :x="col"
+          :y="row"
+      />
     </el-col>
   </el-row>
 </template>
 
 <script setup>
-  import * as echarts from 'echarts'
-  import { ref, computed } from 'vue'
-  import { useResizeObserver } from "@vueuse/core"
-  import DataChart from '~/components/DataChart.vue'
-  import PageCenterTitle from '~/components/PageCenterTitle.vue'
-  import { useIndexStore } from '~/store/useIndexStore.js'
+import * as echarts from 'echarts'
+import {storeToRefs} from 'pinia'
+import {ref, onMounted} from 'vue'
+import {useResizeObserver} from "@vueuse/core"
+import DataChart from '~/components/DataChart.vue'
+import PageCenterTitle from '~/components/PageCenterTitle.vue'
+import {useIndexStore} from '~/store/useIndexStore.js'
 
-  const activeIndex2 = ref('1')
+const activeIndex2 = ref('1')
 
-  const store = useIndexStore()
+const store = useIndexStore()
 
-  store.getHomeContentData()
+store.getHomeContentData()
 
-  const homeContentData  = computed(() => store.homeContentData)
+const {homeContentData} = storeToRefs(store)
 
 </script>
 
 <style scoped>
-  .el-header {
-    padding: 0 0;
-  }
+.el-header {
+  padding: 0 0;
+}
 
-  .demonstration {
-    color: var(--el-text-color-secondary);
-  }
+.demonstration {
+  color: var(--el-text-color-secondary);
+}
 
-  .bodyleft {
-    @apply flex text-lg mx-12 my-4;
-    text-indent: 2em;
-  }
+.bodyleft {
+  @apply flex text-lg mx-12 my-4;
+  text-indent: 2em;
+}
 
-  .bodyright {
-    @apply my-4;
+.bodyright {
+  @apply my-4;
 
-  }
+}
 
-  .all-charts {
-    @apply my-8
-  }
+.all-charts {
+  @apply my-8
+}
 
-  .charts-card {
-    @apply my-4;
-  }
+.charts-card {
+  @apply my-4;
+}
 
-  .data-text {
-    border-bottom: 2px solid #E5E7EB;
-    border-top: 2px solid #E5E7EB;
-    @apply flex justify-center items-center py-3 my-5
-  }
+.data-text {
+  border-bottom: 2px solid #E5E7EB;
+  border-top: 2px solid #E5E7EB;
+  @apply flex justify-center items-center py-3 my-5
+}
 
-  .el-card {
-    --el-card-padding: 10px;
-  }
+.el-card {
+  --el-card-padding: 10px;
+}
 
-  .el-carousel__item {
-    @apply flex justify-center items-center
-  }
+.el-carousel__item {
+  @apply flex justify-center items-center
+}
 
-  .el-carousel__item h3 {
-    @apply text-5xl
-  }
+.el-carousel__item h3 {
+  @apply text-5xl
+}
 </style>
