1a2988fcf02eeab83e519692c7521eccd03171ed
sfc9982 <26681591+sfc9982@users.noreply.github.com>
2023-03-22 14:35:53 +0800

    Improve Implements

--- src/components/DataChart.vue
+++ src/components/DataChart.vue
@@ -56,124 +56,7 @@ onMounted(async () => {
     },
     series: [
       {
-        name: 'Access From',
-        type: 'pie',
-        center: ['50%', '60%'],
-        radius: ['50%', '70%'],
-        avoidLabelOverlap: false,
-        itemStyle: {
-          borderRadius: 2,
-          borderColor: '#fff',
-          borderWidth: 2
-        },
-        label: {
-          show: false,
-          position: 'center'
-        },
-        data: [
-          {value: 100, name: 'Search Engine'},
-          {value: 735, name: 'Direct'},
-          {value: 580, name: 'Email'},
-          {value: 484, name: 'Union Ads'},
-          {value: 300, name: 'Video Ads'}
-        ]
-      }
-    ]
-
-  };
-
-  let x = props.x
-  let y = props.y
-  let id = x - 1 + y * 3 - 3
-  console.log(id)
-
-  // echart 配置模板 开始
-
-  let optionBar = { // 柱状图
-    title: {
-      text: 'Stress Type Count',
-      left: 'center',
-      top: 0,
-      textStyle: {
-        color: '#000'
-      }
-    },
-    tooltip: {
-      trigger: 'axis'
-    },
-    legend: {
-      top: '10%',
-      left: 'center',
-    },
-    xAxis: {
-      type: 'category',
-      data: [],
-      axisTick: {
-        alignWithLabel: true
-      },
-      axisLabel: {
-        interval: 0,
-        rotate: 45,
-        fontSize: 12,
-      }
-    },
-    yAxis: {
-      type: 'value'
-    },
-    toolbox: {
-      show: true,
-      itemSize: 10,
-      feature: {
-        dataView: {
-          readOnly: true
-        },
-        restore: {},
-        saveAsImage: {}
-      }
-    },
-    series: [
-      {
-        name: 'Gene Count',
-        type: 'bar',
-        data: [],
-        barWidth: '60%',
-        barGap: '0%',
-        barCategoryGap: '0%',
-      }
-    ]
-  };
-
-  let optionPie = { // 饼图
-    title: {
-      text: 'Define a name!',
-      left: 'center',
-      top: 0,
-      textStyle: {
-        color: '#000'
-      }
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      top: '10%',
-      left: 'center',
-
-    },
-    toolbox: {
-      show: true,
-      itemSize: 10,
-      feature: {
-        dataView: {
-          readOnly: true
-        },
-        restore: {},
-        saveAsImage: {}
-      }
-    },
-    series: [
-      {
-        name: 'Access From',
+        name: 'Count',
         type: 'pie',
         center: ['50%', '60%'],
         radius: ['50%', '70%'],
@@ -193,110 +76,49 @@ onMounted(async () => {
 
   };
 
-
-  // echart 配置模板 结束
+  let x = props.x
+  let y = props.y
+  let id = x - 1 + y * 3 - 3
+  // console.log(id)
 
   switch (id) {
     case 0: // 数据包含的物种所占有抗性基因的数量
-      option = optionBar
-      try {
-        const res = await axios.post(
-            'http://175.178.9.163:8093/plant-details/plantGeneCount'
-        );
-        let data = res.data.data
-        option.series[0].data = data.list.map((item) => item.value)
-        option.xAxis.data = data.list.map((item) => item.name)
-        option && myChart.setOption(option)
-        myChart.setOption({
-          dataZoom: {
-            type: 'slider', // 支持手动滑动和缩放
-            start: 0,
-            end: 25
-          }
-        });
-      } catch (e) {
-        console.error(e)
-      }
+      renderChart(barConfig('Stress Type Count'), 'http://175.178.9.163:8093/plant-details/plantGeneCount')
+      myChart.setOption({
+        dataZoom: {
+          type: 'slider', // 支持手动滑动和缩放
+          start: 0,
+          end: 25
+        }
+      });
       break
     case 1: // 各胁迫类型基因的统计数据比例
-      option = optionPie
-      option.title.text = 'Stress Type Count'
-      try {
-        const res = await axios.post('http://175.178.9.163:8093/plant-details/StressTypeCount');
-        let data = res.data.data
-        option.series[0].data = data.list.map((item) => {
-          return {
-            value: item.value,
-            name: item.name
-          }
-        })
-        myChart.on('click', function (params) {
-          const url = '/browse/stresstype/list?stressName='
-          router.push(url + params.name.replaceAll(' ', '+'))
-        });
-
-        option && myChart.setOption(option)
-      } catch
-          (e) {
-        console.error(e)
-      }
+      renderChart(pieConfig('Stress Type Count', ['50%', '70%'], ['50%', '65%']), 'http://175.178.9.163:8093/plant-details/StressTypeCount')
+      myChart.on('click', function (params) {
+        const url = '/browse/stresstype/list?stressName='
+        router.push(url + params.name.replaceAll(' ', '+'))
+      });
       break
     case 2: // 各基因家族的占比
-      option = optionPie
-      option.title.text = 'Gene Family Count'
-      try {
-        const res = await axios.post(
-            'http://175.178.9.163:8093/plant-details/GeneFamilyCount'
-        );
-        let data = res.data.data
-        option.series[0].data = data.list.map((item) => {
-          return {
-            value: item.value,
-            name: item.name
-          }
-        })
-        option && myChart.setOption(option)
-      } catch (e) {
-        console.error(e)
-      }
+      renderChart(pieConfig('Gene Family Count', ['50%', '70%'], ['50%', '65%']), 'http://175.178.9.163:8093/plant-details/GeneFamilyCount')
       break
     case 3: // 大类表型出现的概率
-      option = optionPie
-      option.title.text = 'Phenotype Count'
-      option.series[0].radius = ['35%', '50%']
-      option.series[0].center = ['50%', '70%']
-      try {
-        const res = await axios.post(
-            'http://175.178.9.163:8093/plant-details/PhenotypeCount'
-        );
-        let data = res.data.data
-        option.series[0].data = data.list.map((item) => {
-          return {
-            value: item.value,
-            name: item.name
-          }
-        })
-        option.legend = {
-          top: '10%',
-          left: 'center',
-          orient: 'horizontal',
-          // smaller
-          itemWidth: 10,
-          itemHeight: 10,
-          itemGap: 10,
-          textStyle: {
-            fontSize: 12
-          },
-          // only show the first 10 and show a tip
-          data: data.list.map((item) => item.name).slice(0, 10)
+      option.legend = {
+        top: '10%',
+        left: 'center',
+        orient: 'horizontal',
+        itemWidth: 10,
+        itemHeight: 10,
+        itemGap: 10,
+        textStyle: {
+          fontSize: 12
         }
-        option && myChart.setOption(option)
-      } catch (e) {
-        console.error(e)
       }
+      option && myChart.setOption(option)
+      renderChart(pieConfig('Phenotype Count', ['40%', '60%'], ['50%', '67%']), 'http://175.178.9.163:8093/plant-details/PhenotypeCount')
       break
     case 4:
-      renderChart(pieConfig('Expression Organs Gene Count', ['50%', '70%'], ['50%', '65%']), 'http://175.178.9.163:8093/plant-details/expressionOrgansGeneCount')
+      renderChart(pieConfig('Expression Organs Gene Count'), 'http://175.178.9.163:8093/plant-details/expressionOrgansGeneCount')
       break
     case 5:
       renderChart(barConfig('SubCellular Localization Count'), 'http://175.178.9.163:8093/plant-details/subCellularLocalizationCount')
@@ -335,16 +157,8 @@ function renderChart(config, url) {
             config.xAxis.data = data.list.map(item => item.name)
           }
 
-          // if (config.series && config.series[0] && config.series[0].radius) {
-          //   config.series[0].radius = ['35%', '50%']
-          // }
-          //
-          // if (config.series && config.series[0] && config.series[0].center) {
-          //   config.series[0].center = ['50%', '70%']
-          // }
-
           if (config.legend) {
-            config.legend.data = data.list.map(item => item.name).slice(0, 10)
+            config.legend.data = data.list.map(item => item.name).slice(0, 9)
           }
 
           myChart.setOption(option)
@@ -379,7 +193,7 @@ function pieConfig(title = '', radius = ['50%', '70%'], center = ['50%', '60%'])
     },
     series: [
       {
-        name: 'Access From',
+        name: 'Count',
         type: 'pie',
         center: center,
         radius: radius,
