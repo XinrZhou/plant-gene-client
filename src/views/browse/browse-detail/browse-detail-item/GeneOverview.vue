f9c39af2a075d273a2beeea8b3fd4dee30b1f4eb
sfc9982 <26681591+sfc9982@users.noreply.github.com>
2023-06-02 13:27:06 +0800

    Add: auto fetch current protein

--- src/views/browse/browse-detail/browse-detail-item/GeneOverview.vue
+++ src/views/browse/browse-detail/browse-detail-item/GeneOverview.vue
@@ -2,42 +2,43 @@
     <el-row>
         <el-col :xs="24" :sm="24" :md="24" :lg="13" :xl="13" class="page-info page-title">
             <el-icon>
-                <Histogram />
-            </el-icon>Sample overview
+                <Histogram/>
+            </el-icon>
+            Sample overview
         </el-col>
     </el-row>
 
     <el-card shadow="hover">
         <div class="card-nav">
-            <h1>{{geneName}}</h1>
+            <h1>{{ geneName }}</h1>
             <p>GenBank:</p>
             <p>GenBank Locus:</p>
-            <p>pmid:{{geneInfo.pmid}}</p>
+            <p>pmid:{{ geneInfo.pmid }}</p>
         </div>
         <!-- 基因卡片表格 -->
         <div class="description-list">
             <el-descriptions border :column="4">
                 <el-descriptions-item label="Gene name" label-align="right" align="center" label-class-name="my-label"
-                    class-name="my-content" width="150px">
-                    {{geneInfo.gene}}
+                                      class-name="my-content" width="150px">
+                    {{ geneInfo.gene }}
                 </el-descriptions-item>
                 <el-descriptions-item label="TF" label-align="right" align="center">
-                    {{geneInfo.transcriptionFactor}}
+                    {{ geneInfo.transcriptionFactor }}
                 </el-descriptions-item>
                 <el-descriptions-item label="Gene product" label-align="right" align="center" :span="2">
-                    {{geneInfo.geneProduct}}
+                    {{ geneInfo.geneProduct }}
                 </el-descriptions-item>
                 <el-descriptions-item label="Phenotype Influenced" label-align="right" align="center" :span="2">
-                    {{geneInfo.phenotypeInfluenced}}
+                    {{ geneInfo.phenotypeInfluenced }}
                 </el-descriptions-item>
                 <el-descriptions-item label="Subcellular Localization" label-align="right" align="center">
-                    {{geneInfo.subCellularLocalization}}
+                    {{ geneInfo.subCellularLocalization }}
                 </el-descriptions-item>
                 <el-descriptions-item label="Expression Organs/ Location" label-align="right" align="center" :span="2">
-                    {{geneInfo.expressionOrgans}}
+                    {{ geneInfo.expressionOrgans }}
                 </el-descriptions-item>
                 <el-descriptions-item label="Description" label-align="right" align="center">
-                    {{geneInfo.description}}
+                    {{ geneInfo.description }}
                 </el-descriptions-item>
             </el-descriptions>
         </div>
@@ -45,10 +46,10 @@
         <div class="bottom-collapse">
             <el-collapse @change="handleChange" accordion>
                 <el-collapse-item title="Click here to view the DNA sequence" name="1">
-                    {{geneInfo.aminoAcidSequence}}
+                    {{ geneInfo.aminoAcidSequence }}
                 </el-collapse-item>
                 <el-collapse-item title="Click here to view the Protein sequence" name="2">
-                    {{geneInfo.nucleotideSequence}}
+                    {{ geneInfo.nucleotideSequence }}
                 </el-collapse-item>
             </el-collapse>
         </div>
@@ -62,7 +63,7 @@
                 <el-col :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
                     <template v-for="(item, index) in goBiological" :key="index">
                         <p>
-                            {{item?.name }}
+                            {{ item?.name }}
                             <el-button size="small">
                                 <a :href=item.goId target="_blank">Source:data</a>
                             </el-button>
@@ -77,7 +78,7 @@
                 <el-col :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
                     <template v-for="(item, index) in goMolecular" :key="index">
                         <p>
-                            {{item?.name }}
+                            {{ item?.name }}
                             <el-button size="small">
                                 <a :href=item.goId target="_blank">Source:data</a>
                             </el-button>
@@ -92,7 +93,7 @@
                 <el-col :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
                     <template v-for="(item, index) in goCellur" :key="index">
                         <p>
-                            {{item?.name }}
+                            {{ item?.name }}
                             <el-button size="small">
                                 <a :href=item.goId target="_blank">Source:data</a>
                             </el-button>
@@ -104,8 +105,8 @@
         <div class="card-nav" v-if="geneKeGGInfo.length != 0">
             <h1>KeGG</h1>
             <el-table :data="geneKeGGInfo" height="250" style="width: 100%">
-                <el-table-column prop="description" label="Description" width="300" />
-                <el-table-column prop="koNumber" label="KoNumber" width="200" />
+                <el-table-column prop="description" label="Description" width="300"/>
+                <el-table-column prop="koNumber" label="KoNumber" width="200"/>
                 <el-table-column fixed="right" label="Link" width="200">
                     <template #default="scope">
                         <el-button size="small">
@@ -118,105 +119,187 @@
                 </el-table-column>
             </el-table>
         </div>
+        <div class="card-nav">
+            <h1>3D Protein View of {{ geneName }}</h1>
+            <!--            <h2>{{ seqMd5 }}</h2>-->
+            <!--            <h2>PID: {{ pid }} SID: {{ sid }}</h2>-->
+            <el-row justify="start">
+                <el-col :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
+                    <h3>Protein Accession ID:</h3>
+                </el-col>
+                <el-col :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
+                    <p>
+                        {{ pid }}
+                    </p>
+                </el-col>
+            </el-row>
+            <el-row justify="start">
+                <el-col :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
+                    <h3>Protein Structure ID (PDB/AlphaFold):</h3>
+                </el-col>
+                <el-col :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
+                    <p>
+                        {{ sid }}
+                    </p>
+                </el-col>
+            </el-row>
+            <nightingale-structure :protein-accession=pid :structure-id=sid></nightingale-structure>
+            <h2>W/S/A/D: Move Camera</h2>
+            <h2>Mouse Wheel: Zoom</h2>
+            <h2>LMB: Select Component</h2>
+            <h2>RMB: Move Protein</h2>
+            <br/>
+            <h2>Guest from Mainland China may meets loading issue for 3D view.</h2>
+            <!--            <nightingale-structure protein-accession="A0A804LV25" structure-id="AF-A0A804LV25-F1"/>-->
+        </div>
     </el-card>
-
 </template>
 
 <script setup>
-    import PageLeftTitle from '~/components/PageLeftTitle.vue'
-    import { useRoute } from 'vue-router'
-    import { Histogram } from '@element-plus/icons-vue'
-    import { useBrowseStore } from '~/store/useBrowseStore.js'
-    import { storeToRefs } from 'pinia'
-    import { computed } from 'vue'
+import {useRoute} from 'vue-router'
+import {Histogram} from '@element-plus/icons-vue'
+import {useBrowseStore} from '~/store/useBrowseStore.js'
+import {computed, reactive, toRefs} from 'vue'
+import '@nightingale-elements/nightingale-structure/dist/index.js'
+import {reqGetGeneOverview} from "~/api/browse.js";
+import md5 from 'md5'
 
-    const route = useRoute()
-    const store = useBrowseStore()
-    const geneInfo = computed(() => store.geneOverviewDataList)
+const route = useRoute()
+const store = useBrowseStore()
+const geneInfo = computed(() => store.geneOverviewDataList)
 
-    const geneKeGGInfo = computed(() => store.geneKeGGList)
-    const geneGoInfo = computed(() => store.geneGoList)
-    const goBiological = computed(() => geneGoInfo.value.filter(item => item.ontology == 'biological_process'))
-    const goMolecular = computed(() => geneGoInfo.value.filter(item => item.ontology == 'molecular_function'))
-    const goCellur = computed(() => geneGoInfo.value.filter(item => item.ontology == 'cellur_component'))
+const geneKeGGInfo = computed(() => store.geneKeGGList)
+const geneGoInfo = computed(() => store.geneGoList)
+const goBiological = computed(() => geneGoInfo.value.filter(item => item.ontology == 'biological_process'))
+const goMolecular = computed(() => geneGoInfo.value.filter(item => item.ontology == 'molecular_function'))
+const goCellur = computed(() => geneGoInfo.value.filter(item => item.ontology == 'cellur_component'))
 
-    const geneName = route.query.geneName
-    store.getGeneOverviewData(geneName)
-    store.getGeneByKeGGData(geneName)
-    store.getGeneByGoData(geneName)
-    // API调用
-    let handleChange = (event) => {
-        console.log(event)
-    }
+const geneName = route.query.geneName
+store.getGeneOverviewData(geneName)
+store.getGeneByKeGGData(geneName)
+store.getGeneByGoData(geneName)
+// API调用
+let handleChange = (event) => {
+    console.log(event)
+}
+
+const state = reactive({
+    pid: '',
+    sid: ''
+})
+
+let {pid, sid} = toRefs(state)
+
+let geneSeq = ""
+let seqMd5 = ""
+reqGetGeneOverview(geneName).then(res => {
+    geneSeq = res.data.aminoAcidSequence
+    console.log(geneSeq)
+    geneSeq = geneSeq.split('\n').slice(1).join('')
+    console.log(geneSeq)
+    seqMd5 = md5(geneSeq)
+    console.log(seqMd5)
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    };
+
+    fetch(`https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=1&md5=${seqMd5}`, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data[0])
+            state.pid = data[0].accession
+            console.log(state.pid)
+            fetch(`https://rest.uniprot.org/uniprotkb/search?query=accession:${state.pid}&fields=structure_3d,xref_alphafolddb`, options)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.results[0])
+                    const struct_data = data.results[0]
+                    state.sid = state.pid
+                    if (struct_data.uniProtKBCrossReferences[0].database === "AlphaFoldDB") {
+                        state.sid = "AF-" + struct_data.uniProtKBCrossReferences[0].id + "-F1"
+                    } else if (struct_data.uniProtKBCrossReferences[0].database === "PDB") {
+                        state.sid = struct_data.uniProtKBCrossReferences[0].id
+                    }
+                })
+                .catch(error => console.log('RESTful EBI API Error: ', error))
+        })
+        .catch(error => console.log('EBI API Error: ', error))
+    // state.pid = 'A0A804LV25'
+    // state.sid = 'AF-A0A804LV25-F1'
+})
 
 </script>
 
 <style scoped>
-    @media screen and (min-width: 992px) {
-        .el-card {
-            max-width: 70%;
-            margin: 0 auto;
-            margin-bottom: 30px;
-        }
-
-        .el-row {
-            max-width: 70%;
-        }
-
-        .page-title {
-            margin: 0 auto;
-        }
+@media screen and (min-width: 992px) {
+    .el-card {
+        max-width: 70%;
+        margin: 0 auto;
+        margin-bottom: 30px;
     }
 
-    /* 手机端 */
-    @media screen and (max-width: 993px) {
-        .el-card {
-            max-width: 100%;
-        }
-
-        .el-row {
-            max-width: 70%;
-            margin: 0 auto;
-        }
-
-        .page-title {
-            max-width: 100%;
-        }
+    .el-row {
+        max-width: 70%;
     }
 
-    .page-info {
-        @apply flex items-center font-bold pt-6 pb-6 text-4xl my-6;
+    .page-title {
+        margin: 0 auto;
+    }
+}
+
+/* 手机端 */
+@media screen and (max-width: 993px) {
+    .el-card {
+        max-width: 100%;
     }
 
-    .card-nav p {
-        @apply flex items-center font-normal py-1 text-sm mx-11 tracking-wide;
+    .el-row {
+        max-width: 70%;
+        margin: 0 auto;
     }
 
-    .el-button {
-        @apply ml-3 bg-slate-100;
+    .page-title {
+        max-width: 100%;
     }
+}
 
-    .card-nav span {
-        @apply font-normal py-2 pb-3 text-base mx-11 tracking-wide;
-    }
+.page-info {
+    @apply flex items-center font-bold pt-6 pb-6 text-4xl my-6;
+}
 
-    .card-nav h1 {
-        @apply flex items-center font-semibold py-5 text-2xl mx-11;
-    }
+.card-nav p {
+    @apply flex items-center font-normal py-1 text-sm mx-11 tracking-wide;
+}
 
-    .card-nav h3 {
-        @apply flex items-center font-semibold py-1 text-base mx-11;
-    }
+.el-button {
+    @apply ml-3 bg-slate-100;
+}
 
-    .description-list {
-        @apply flex-auto justify-self-center mx-4 my-6;
-    }
+.card-nav span {
+    @apply font-normal py-2 pb-3 text-base mx-11 tracking-wide;
+}
 
-    .bottom-collapse {
-        @apply mx-4 my-6;
-    }
+.card-nav h1 {
+    @apply flex items-center font-semibold py-5 text-2xl mx-11;
+}
 
-    .el-table {
-        @apply mx-11;
-    }
+.card-nav h3 {
+    @apply flex items-center font-semibold py-1 text-base mx-11;
+}
+
+.description-list {
+    @apply flex-auto justify-self-center mx-4 my-6;
+}
+
+.bottom-collapse {
+    @apply mx-4 my-6;
+}
+
+.el-table {
+    @apply mx-11;
+}
 </style>
