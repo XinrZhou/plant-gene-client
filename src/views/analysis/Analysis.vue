7457d5e99678bdc5c79e809effdb4932e23842e0
liuwuyong <1140752286@qq.com>
2023-06-10 19:07:49 +0800

    fix

--- src/views/analysis/Analysis.vue
+++ src/views/analysis/Analysis.vue
@@ -16,7 +16,7 @@
                   <el-form :model="form" label-width="120px" label-position="left">
                     <el-form-item label="Sequence">
                       <el-input v-model="form.sequence" :autosize="{ minRows: 10, maxRows: 12 }"
-                        :disabled="fileCount > 0 ? true : false" type="textarea" style="width: 70%;" placeholder="Please enter the nucleotide or amino acid sequence and select the corresponding 'blast' type.&#13;
+                                :disabled="fileCount > 0 ? true : false" type="textarea" style="width: 70%;" placeholder="Please enter the nucleotide or amino acid sequence and select the corresponding 'blast' type.&#13;
 >Gene1
 MDPFYTSFSDSFLSIPDHRSPVSDSSECSPKLASSCPKKRAGRKKFRETRHPIYRGVRQRNSGKWVCEVR
 EPNKKSRIWLGTFPTVEMAARAHDVAALALRGRSACLNFADSAWRLRIPESTCPKEIQKAAAEAAMAFQN
@@ -31,7 +31,7 @@ SLWSF" />
                     </el-form-item>
                     <el-form-item label="Select File">
                       <el-upload ref="uploadRef" :file-list="fileList" :auto-upload="false" :on-change="fileChange"
-                        :on-remove="fileRemove" class="upload-demo" :limit="1" :on-exceed="handleExceed">
+                                 :on-remove="fileRemove" class="upload-demo" :limit="1" :on-exceed="handleExceed">
                         <el-button class="form-btn">Click to upload</el-button>
                         <template #tip>
                           <div class="el-upload__tip">
@@ -55,11 +55,11 @@ SLWSF" />
               <el-card shadow="hover">
                 <el-descriptions :column="1" border>
                   <el-descriptions-item label="Blast Version"
-                    label-align="left">{{blastSeqInfo.blastVersion}}</el-descriptions-item>
+                                        label-align="left">{{blastSeqInfo.blastVersion}}</el-descriptions-item>
                   <el-descriptions-item label="Database"
-                    label-align="left">{{blastSeqInfo.database}}</el-descriptions-item>
+                                        label-align="left">{{blastSeqInfo.database}}</el-descriptions-item>
                   <el-descriptions-item label="QuerySeq Length"
-                    label-align="left">{{blastSeqInfo.length}}</el-descriptions-item>
+                                        label-align="left">{{blastSeqInfo.length}}</el-descriptions-item>
                   <el-descriptions-item label="Query" label-align="left">{{blastSeqInfo.query}}</el-descriptions-item>
                 </el-descriptions>
                 <el-tabs v-model="activeName" type="border-card">
@@ -71,7 +71,7 @@ SLWSF" />
                       <el-table-column prop="querySeqId" label="QuerySeqId">
                         <template v-slot="{ row }">
                           <a href="" @click.prevent="handleGeneClick(row)"
-                            class="underline font-bold">{{row.querySeqId}}</a>
+                             class="underline font-bold">{{row.querySeqId}}</a>
                         </template>
                       </el-table-column>
                       <el-table-column prop="beginDbSeqNumber" label="StressType" />
@@ -85,13 +85,13 @@ SLWSF" />
                       <el-table-column prop="endQuerySeqNumber" label="Gene">
                         <template v-slot="{ row }">
                           <a href="" @click.prevent="handleGeneClick1(row)"
-                            class="underline font-bold">{{row.endQuerySeqNumber}}</a>
+                             class="underline font-bold">{{row.endQuerySeqNumber}}</a>
                         </template>
                       </el-table-column>
                       <el-table-column prop="dbSeqId" label="PlantASRGId">
                         <template v-slot="{ row }">
                           <a href="" @click.prevent="handleGeneClick(row)"
-                            class="underline font-bold">{{row.dbSeqId}}</a>
+                             class="underline font-bold">{{row.dbSeqId}}</a>
                         </template>
                       </el-table-column>
                       <el-table-column prop="" label="" />
@@ -175,185 +175,185 @@ SLWSF" />
 </template>
 
 <script setup>
-  import PageLeftTitle from '~/components/PageLeftTitle.vue'
-  import { reactive, toRaw, ref, computed } from 'vue'
-  import { useSubmitStore } from '~/store/useSubmitStore.js'
-  import { ElMessageBox, ElMessage } from 'element-plus'
-  import { showModal } from "~/composables/util.js";
-  import { useFileStore } from "~/store/useFileStore.js";
-  import router from "~/router/index.js";
+import PageLeftTitle from '~/components/PageLeftTitle.vue'
+import { reactive, toRaw, ref, computed } from 'vue'
+import { useSubmitStore } from '~/store/useSubmitStore.js'
+import { ElMessageBox, ElMessage } from 'element-plus'
+import { showModal } from "~/composables/util.js";
+import { useFileStore } from "~/store/useFileStore.js";
+import router from "~/router/index.js";
 
-  const store = useSubmitStore()
-  const storeFiles = useFileStore()
-  let form = ref({
-    sequence: '',
-    category: 'blastn'
+const store = useSubmitStore()
+const storeFiles = useFileStore()
+let form = ref({
+  sequence: '',
+  category: 'blastn'
+})
+let formData = new FormData()
+
+let fileList = reactive([])
+let fileCount = ref(0)
+let uploadRef = ref()
+let submitting = ref(false)
+let blastSeqList = computed(() => store.BlastSeqDataList.list)
+let blastSeqInfo = computed(() => store.BlastSeqDataList.mainInfo)
+let blastSeqReslt = computed(() => store.BlastSeqDataList.result)
+const activeName = ref('first')
+
+const fileChange = (file, lists) => {
+  let list = toRaw(lists)
+  fileCount.value = list.length
+  list.forEach(item => {
+    formData.set('file', item.raw, item.name)
   })
-  let formData = new FormData()
+}
 
-  let fileList = reactive([])
-  let fileCount = ref(0)
-  let uploadRef = ref()
-  let submitting = ref(false)
-  let blastSeqList = computed(() => store.BlastSeqDataList.list)
-  let blastSeqInfo = computed(() => store.BlastSeqDataList.mainInfo)
-  let blastSeqReslt = computed(() => store.BlastSeqDataList.result)
-  const activeName = ref('first')
+const handleExceed = (files, lists) => {
+  uploadRef.value.clearFiles()  //清空上传文件（限制一个，所以直接清空即可）
+  const file = files[0]
+  uploadRef.value.handleStart(file)  //重新上传
+}
 
-  const fileChange = (file, lists) => {
-    let list = toRaw(lists)
-    fileCount.value = list.length
-    list.forEach(item => {
-      formData.set('file', item.raw, item.name)
+const fileRemove = () => {
+  fileCount.value = fileCount.value - 1
+  uploadRef.value.clearFiles()
+}
+
+const onSubmit = () => {
+  submitting.value = true
+
+  // onReset()
+  if (fileCount.value > 0) {
+    store.reqPostUploadBlastFile(form.value, formData).then(() => {
+      submitting.value = false
+      ElMessage({
+        message: 'blast Successfully!',
+        type: 'success',
+      })
+    }).catch((err) => {
+      submitting.value = false
+      console.error(err)
+    })
+  } else {
+    store.reqPostUploadBlastSeq(form.value).then(() => {
+      submitting.value = false
+      ElMessage({
+        message: 'blast Successfully!',
+        type: 'success',
+      })
+    }).catch((err) => {
+      submitting.value = false
+      console.error(err)
     })
   }
+}
 
-  const handleExceed = (files, lists) => {
-    uploadRef.value.clearFiles()  //清空上传文件（限制一个，所以直接清空即可）
-    const file = files[0]
-    uploadRef.value.handleStart(file)  //重新上传
+const onReset = () => {
+  uploadRef.value.clearFiles()
+  formData = new FormData()
+  fileCount.value = 0
+  form.value = {
+    sequence: '',
+    category: ''
   }
-
-  const fileRemove = () => {
-    fileCount.value = fileCount.value - 1
-    uploadRef.value.clearFiles()
-  }
-
-  const onSubmit = () => {
-    submitting.value = true
-
-    // onReset()
-    if (fileCount.value > 0) {
-      store.reqPostUploadBlastFile(form.value, formData).then(() => {
-        submitting.value = false
-        ElMessage({
-          message: 'blast Successfully!',
-          type: 'success',
-        })
-      }).catch((err) => {
-        submitting.value = false
-        console.error(err)
-      })
-    } else {
-      store.reqPostUploadBlastSeq(form.value).then(() => {
-        submitting.value = false
-        ElMessage({
-          message: 'blast Successfully!',
-          type: 'success',
-        })
-      }).catch((err) => {
-        submitting.value = false
-        console.error(err)
-      })
-    }
-  }
-
-  const onReset = () => {
-    uploadRef.value.clearFiles()
-    formData = new FormData()
-    fileCount.value = 0
-    form.value = {
-      sequence: '',
-      category: ''
-    }
-  }
-  const handleDownload = (category) => {
-    showModal("Are you sure to download?？").then(res => {
-      storeFiles.getFilesByIdAndName(blastSeqInfo.value.id, category).then(response => {
-        console.log(blastSeqInfo.value.id)
-        console.log(category)
-        console.log(response)
-        const contentDisposition = response.headers['content-disposition'];
-        const fileName = contentDisposition
+}
+const handleDownload = (category) => {
+  showModal("Are you sure to download?？").then(res => {
+    storeFiles.getFilesByIdAndName(blastSeqInfo.value.id, category).then(response => {
+      console.log(blastSeqInfo.value.id)
+      console.log(category)
+      console.log(response)
+      const contentDisposition = response.headers['content-disposition'];
+      const fileName = contentDisposition
           .match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1]
           .replace(/['"]/g, '');
 
-        const blob = new Blob([response.data]);
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', fileName);
-        document.body.appendChild(link);
-        link.click();
-      });
-    })
+      const blob = new Blob([response.data]);
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', fileName);
+      document.body.appendChild(link);
+      link.click();
+    });
+  })
 
-  }
-  let handleGeneClick = (row) => {
-    const link = document.createElement('a');
-    link.href = "https://www.ncbi.nlm.nih.gov/nucleotide/" + row.dbSeqId
-    link.click();
-    link.preventDefault();
-  }
-  let handleGeneClick1 = (row) => {
-    router.push({
-      name: 'geneoverview',
-      query: {
-        geneName: row.beginQuerySeqNumber
-      }
-    })
-  }
+}
+let handleGeneClick = (row) => {
+  const link = document.createElement('a');
+  link.href = "https://www.ncbi.nlm.nih.gov/nucleotide/" + row.dbSeqId
+  link.click();
+  link.preventDefault();
+}
+let handleGeneClick1 = (row) => {
+  router.push({
+    name: 'geneoverview',
+    query: {
+      geneName: row.beginQuerySeqNumber
+    }
+  })
+}
 </script>
 
 <style scoped>
-  /* PC端 */
-  @media screen and (min-width: 992px) {
-    .page-style {
-      max-width: 70%;
-      margin: 0 auto;
-    }
-
-    .blast-result {
-      margin-top: 40px;
-    }
+/* PC端 */
+@media screen and (min-width: 992px) {
+  .page-style {
+    max-width: 70%;
+    margin: 0 auto;
   }
 
-  /* 手机端 */
-  @media screen and (max-width: 993px) {
-    .el-row {
-      max-width: 100%;
-    }
+  .blast-result {
+    margin-top: 40px;
+  }
+}
 
-    .blast-result {
-      margin-top: 40px;
-    }
+/* 手机端 */
+@media screen and (max-width: 993px) {
+  .el-row {
+    max-width: 100%;
   }
 
-  .blast-tip {
-    @apply pt-6 pb-3;
+  .blast-result {
+    margin-top: 40px;
   }
+}
 
-  .blast-form {
-    @apply px-7 py-6 bg-white rounded-md text-left;
-  }
+.blast-tip {
+  @apply pt-6 pb-3;
+}
 
-  .alignments-item {
-    @apply font-semibold mb-2;
-  }
+.blast-form {
+  @apply px-7 py-6 bg-white rounded-md text-left;
+}
 
-  .alignments-sequence {
-    @apply text-sm my-5 leading-6;
-  }
+.alignments-item {
+  @apply font-semibold mb-2;
+}
 
-  .el-tabs {
-    @apply mt-6;
-  }
+.alignments-sequence {
+  @apply text-sm my-5 leading-6;
+}
 
-  .el-button {
-    @apply float-right mr-2 mb-2;
-  }
+.el-tabs {
+  @apply mt-6;
+}
 
-  .form-btn {
-    background-color: #CCD5AE;
-  }
+.el-button {
+  @apply float-right mr-2 mb-2;
+}
 
-  .SaGp-desciption {
-    @apply flex items-center font-bold pb-3 text-lg;
-  }
+.form-btn {
+  background-color: #CCD5AE;
+}
 
-  .link-btn {
-    display: flex;
-    justify-content: center;
-    margin-top: 20px;
-  }
+.SaGp-desciption {
+  @apply flex items-center font-bold pb-3 text-lg;
+}
+
+.link-btn {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
 </style>
